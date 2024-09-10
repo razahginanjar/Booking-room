d4425cb5f3b407454b82fac238fa99e3bd8b44a6
Carlos Richard Geraldine <carlgeralz.ofc@gmail.com>
2024-09-10 12:22:17 +0700

    [FIX] add equals method for test in CommonResponse

--- src/main/java/com/enigma/challengebookingroom/dto/response/CommonResponse.java
+++ src/main/java/com/enigma/challengebookingroom/dto/response/CommonResponse.java
@@ -1,5 +1,7 @@
 package com.enigma.challengebookingroom.dto.response;
 
+import java.util.Objects;
+
 import lombok.*;
 
 @Getter
@@ -11,36 +13,21 @@ public class CommonResponse<T> {
     private int statusCode;
     private String message;
     private T data;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CommonResponse<?> that = (CommonResponse<?>) o;
+        return statusCode == that.statusCode &&
+                Objects.equals(message, that.message) &&
+                Objects.equals(data, that.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(statusCode, message, data);
+    }
 }
 
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CommonResponse<?> that = (CommonResponse<?>) o;
-        return statusCode == that.statusCode &&
-                Objects.equals(message, that.message) &&
-                Objects.equals(data, that.data);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(statusCode, message, data);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CommonResponse<?> that = (CommonResponse<?>) o;
-        return statusCode == that.statusCode &&
-                Objects.equals(message, that.message) &&
-                Objects.equals(data, that.data);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(statusCode, message, data);
-    }
 // kalo mau nambahin paging ntar tambahin sendiri kesini juga hehe
